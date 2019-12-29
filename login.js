71b62533c349e818fe3787bc5b50e7c63facdef5
misaelsarm <misaelsarmiento97@gmail.com>
2019-12-28 18:44:06 -0600

    added initial files

---
+++ login.js
@@ -0,0 +1,25 @@
+//login form
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+        const userEmail = cred.user.email;
+        alert("Bienvenido " + userEmail)
+        window.location.href = "index.html"
+    })
+})
+
+//listen for auth status changes
+auth.onAuthStateChanged(user => {
+    if (user) {
+        window.location.href = "index.html"
+
+    } else {
+
+        //window.location.href = "login.html"
+        // window.location.replace = "login.html"
+        console.log(user);
+    }
+})
---
+++ main.js
@@ -0,0 +1,20 @@
+//logout
+const logOut = document.querySelector('#logOut');
+logOut.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut();
+    window.location.href = "login.html"
+})
+
+//listen for auth status changes
+auth.onAuthStateChanged(user => {
+    if (user) {
+        console.log(user.email);
+        const welcome = document.querySelector('#welcome')
+        const name = `Bienvenido <b>${user.email}</b>`
+        welcome.innerHTML = name
+    } else {
+        console.log(user);
+        window.location.href = "login.html"
+    }
+})
